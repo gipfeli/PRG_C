6ad4eb9b526e51ba5e7cc8c2724514df53d94e99
Dat Nguyen <nguyenchanhdat@gmail.com>
2020-03-07 20:44:26 +0100

    Aufg 3 complete.

--- 00_Exercises/03_pascalTriangle/pascalTriangle.c
+++ 00_Exercises/03_pascalTriangle/pascalTriangle.c
@@ -11,17 +11,17 @@ int main (int argc, char* argv[]){
         printf("Usage: multiTable wholeNumber\n wholeNumber: integer on the interval [1,10]\n");
     }
 
-    for (i=0; i < rows; i++){
-        for (space=1; space <= rows-i; space++){
-            printf(" ");
-        }
+    for (i=0; i<rows; i++){                       // Display rows
+        for (space=1; space <= rows-i; space++)
+            printf("  ");                         // 2 spaces for better looking
+        
         for (j=0; j <= i; j++){
             if (j==0 || i == 0){
                 coeff = 1;
             }else{
                 coeff = coeff*(i-j+1)/j;
-                printf("% 4d", coeff);
             }
+            printf("%4d", coeff);
         }
         printf("\n");
     }
