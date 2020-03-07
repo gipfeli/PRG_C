8d8c2d139dc63471a093757fc03e22181cc32bd1
Dat Nguyen <nguyenchanhdat@gmail.com>
2020-03-07 18:41:55 +0100

    Teil 2 Auf 2 complete

--- 00_Exercises/02b_multiTableDesc/multiTableDesc.c
+++ 00_Exercises/02b_multiTableDesc/multiTableDesc.c
@@ -6,15 +6,14 @@ int main (int argc, char* argv[]){
     int inputValue = atoi(argv[1]);
     int i,j,n;
 
-    if(argc != 2 && inputValue < 0 || inputValue > 10){
-        printf("Usage: multiTable wholeNumber\n wholeNumber: integer on the interval [1,10]\n");
+    if(argc != 2 && inputValue < 0 || inputValue > 20){
+        printf("Usage: multiTable wholeNumber\n wholeNumber: integer on the interval [1,20]\n");
     }
     for(i=1; i <= inputValue; i++){
-        for(j=1; j <=inputValue; j++){
+        for(j=inputValue; j >=1; j--){
             printf("%d\t ",i*j); 
         }
         printf("\n");
     }
-    
-    return 0;
+        return 0;
 }
