8aaf60bd80bb697fdd0a5422a80afc66a35f23fa
Dat Nguyen <nguyenchanhdat@gmail.com>
2020-03-07 20:23:49 +0100

    Test result

--- 00_Exercises/03_pascalTriangle/pascalTriangle.c
+++ 00_Exercises/03_pascalTriangle/pascalTriangle.c
@@ -3,5 +3,28 @@
 #include <stdlib.h>
 
 int main (int argc, char* argv[]){
-    
+    int rows = atoi(argv[1]);
+    int i,j,space;
+    int coeff = 1;
+
+    if(argc != 2 && rows < 0 || rows > 10){
+        printf("Usage: multiTable wholeNumber\n wholeNumber: integer on the interval [1,10]\n");
+    }
+
+    for (i=0; i < rows; i++){
+        for (space=1; space <= rows-i; space++){
+            printf(" ");
+        }
+        for (j=0; j <= i; j++){
+            if (j==0 || i == 0){
+                coeff = 1;
+            }else{
+                coeff = coeff*(i-j+1)/j;
+                printf("% 4d", coeff);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
