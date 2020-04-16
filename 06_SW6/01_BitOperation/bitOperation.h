f5b400255d18af17ff67a0bbe0e809a48cb62173
Dat Nguyen <nguyenchanhdat@gmail.com>
2020-04-16 19:48:00 +0200

    Bitwise

---
+++ 06_SW6/01_BitOperation/bitOperation.h
@@ -0,0 +1,10 @@
+#ifndef _H_BIT_OPERATION_H
+#define _H_BIT_OPERATION_H
+
+void SetBitNof8(unsigned char* target, unsigned char bitNumber);
+void ClearBitNof8(unsigned char* target, unsigned char bitNumber);
+void FlipBitNof8(unsigned char* target, unsigned char bitNumber);
+unsigned char CheckBitNof8(unsigned char target, unsigned char bitNumber);
+
+
+#endif
---
+++ 06_SW6/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//compile flags
+#define DEBUG
+
+//custom includes
+#include "hello.h"
+
+int main (int argc, char *argv[]){
+	helloWorld();
+
+	return EXIT_SUCCESS;
+}
